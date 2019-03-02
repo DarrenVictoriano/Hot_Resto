b852beb29b20554dcf88f55683a9116919918ebb
Darren Victoriano <darren.victoriano@gmail.com>
2019-03-02 10:59:59 -0800

    initial push

---
+++ server.js
@@ -0,0 +1,51 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Data
+// ===========================================================
+var reservationData = [{
+    routeName: "yoda",
+    name: "Yoda",
+    phone: "721-123-3123",
+    email: "jedi@master.com"
+}, {
+    routeName: "yoda",
+    name: "Yoda",
+    phone: "721-123-3123",
+    email: "jedi@master.com"
+}, {
+    routeName: "yoda",
+    name: "Yoda",
+    phone: "721-123-3123",
+    email: "jedi@master.com"
+}];
+
+// Routes
+// ===========================================================
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "table.html"));
+});
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
